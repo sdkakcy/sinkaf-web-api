d0775b43ea4d351e9b0f0be408796fdfa40a869e
Sıddık Akçay <sdkakcy@gmail.com>
2022-12-04 13:55:22 +0300

    app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from sinkaf import Sinkaf
+
+obj = Sinkaf()
+
+app = Flask(__name__)
+
+@app.route("/tahmin")
+def tahmin():
+    sentence = request.args.get('sentence', '')
+    result = not obj.tahmin([sentence]).tolist()[0]
+    return {
+        "sentence": sentence,
+        "safe": result
+    }
